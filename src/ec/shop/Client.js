7f117cb062e5d35a62049cd8f9599d17ebf4e5d9
Yoshi Aoki <yoshi@1o1.co.jp>
2019-10-19 04:48:48 +0900

    feat: EC Apis

---
+++ src/ec/shop/Client.js
@@ -0,0 +1,77 @@
+/* @flow strict */
+
+import {
+  BaseClient,
+  type Response,
+  type PageResult,
+  type URL,
+} from '../../core'
+
+import type {
+  Key,
+  //
+  Entry,
+} from './types'
+
+/**
+ */
+export type CreateParams = {
+  mall: MallKey,
+  name: Name,
+  uniqueName: Key,
+  url: URL,
+}
+
+/**
+ */
+export type UpdateParams = {
+}
+
+/**
+ *
+ * @class
+ */
+export class Client extends BaseClient {
+
+  get basepath() {
+    return '/ec/shops'
+  }
+
+  /**
+   * List brand entries
+   */
+  listEntries = (size: number = 10, offset: number = 0): Response<PageResult<Entry>> => {
+    return this.httpClient.get(this.path(), {
+      params: {
+        size,
+        offset,
+      },
+    })
+  }
+
+  /**
+   * Describe the brand entry
+   */
+  describeEntry = (key: Key): Response<Entry> => {
+    return this.httpClient.get(this.path(key))
+  }
+
+  /**
+   */
+  createEntry = (params: CreateParams): Response<Entry> => {
+    return this.httpClient.post(this.path(), params)
+  }
+
+  /**
+   */
+  updateEntry = (key: Key, params: UpdateParams): Response<void> => {
+    return this.httpClient.post(this.path(key), params)
+  }
+
+  /**
+   */
+  deleteEntry = (key: Key): Response<void> => {
+    return this.httpClient.delete(this.path(key))
+  }
+}
+
